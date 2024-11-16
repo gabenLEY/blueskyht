85b4b67545f1fef358a6faae76d2c455952ff692
gabenley <gabenley1@gmail.com>
2024-11-16 15:52:15 -0500

    Update in the Transup website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { agent } from "~/lib/api";
 import Home from "~/components/Home";
 export * from '../types/feedTypes';
-import { feedDataType } from "~/types/feedTypes";
 
 
 export default async function Homepage() {
   const feeds: any = await agent.app.bsky.unspecced.getPopularFeedGenerators({
     limit: 100,
   });
-  console.log(feeds)
   return (
     <div className="">
       <Home feedItems={feeds.data} />
